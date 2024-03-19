8f14a4e0b44cb88ea3f841f3ac65fcb338e64b46
Panjianhui <112854369+13429378246@users.noreply.github.com>
2024-03-19 09:43:33 +0800

    2024/3/19

---
+++ CPU/type.v
@@ -0,0 +1,78 @@
+
+module ExampleModule #(
+    parameter R = 7'b0000011,
+    parameter I = 7'b0000111,
+    parameter S = 7'b0001111,
+    parameter B = 7'b0001011,
+    parameter U = 7'b0011011,
+    parameter J = 7'b0011111
+) (
+    in,
+    opcode,
+    imm,
+    func,
+    rs2,
+    rs1,
+    rd
+
+);
+  input [31:0] in;
+  output reg [31:0] imm;
+  output reg [4:0] rs2, rs1, rd;
+  output reg [9:0] func;
+  output wire [6:0] opcode;
+  assign opcode = in[6:0];
+  always @(*) begin
+    case (opcode)
+      R: begin
+        imm  <= 0;
+        func <= {in[31:25], in[14:12]};
+        rs2  <= in[24:20];
+        rs1  <= in[19:15];
+        rd   <= in[11:7];
+      end
+      I: begin
+        imm  <= {{20{in[31]}}, in[31:20]};
+        func <= {7'b0, in[14:12]};
+        rs2  <= 0;
+        rs1  <= in[19:15];
+        rd   <= in[11:7];
+      end
+      S: begin
+        imm  <= {{20{in[31]}}, in[31:25], in[11:7]};
+        func <= {7'b0, in[14:12]};
+        rs2  <= in[24:20];
+        rs1  <= in[19:15];
+        rd   <= 0;
+      end
+      B: begin
+        imm  <= {{20{in[31]}},in[31], in[7], in[30:25], in[11:8]};
+        func <= {7'b0, in[14:12]};
+        rs2  <= in[24:20];
+        rs1  <= in[19:15];
+        rd   <= 0;
+      end
+      U: begin
+        imm  <= {{12{in[31]}}, in[31:12]};
+        func <= 0;
+        rs2  <= 0;
+        rs1  <= 0;
+        rd   <= in[11:7];
+      end
+      J: begin
+        imm  <= {{12{in[31]}}, in[31], in[19:12], in[20], in[30:21]};
+        func <= {in[31:25], in[14:12]};
+        func <= 0;
+        rs2  <= 0;
+        rs1  <= 0;
+        rd   <= in[11:7];
+      end
+
+
+
+    endcase
+  end
+
+
+
+endmodule
